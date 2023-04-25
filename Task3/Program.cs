57a6156249b5672ad99fd6fc682ff36b77308003
bakhtovar <bakhtovarrizozoda@gmail.com>
2023-04-25 20:06:21 +0500

    Egzamin2

---
+++ Task3/Program.cs
@@ -0,0 +1,38 @@
+﻿
+var calcul = new Calculator();
+Console.Write("The first number is: ");
+calcul.A = Convert.ToInt32(Console.ReadLine());
+Console.Write("The first number is: ");
+calcul.n = Convert.ToString(Console.ReadLine());
+Console.Write("The operation is: ");
+calcul.B = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine(calcul.Loop);
+
+
+public class Calculator
+{
+    public int A;
+    public int B;
+    public string n;
+    public void Loop()
+    {
+        if(n == "+")
+        {
+            Console.WriteLine(A + B);
+        } 
+        else if (n == "-")
+        {
+            Console.WriteLine(A - B);
+        }
+        else if(n == "*")
+        {
+            Console.WriteLine(A * B);
+        }
+        else if(n == "/")
+        {
+            Console.WriteLine(A / B);
+        }
+    }
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,33 @@
+﻿
+var us = new User();
+us.Firstname = "Bakhtovar";
+us.Lastname = "Rizozoda";
+us.Username = "Bakhtovar";
+us.Password = 123456;
+
+Console.WriteLine(us.Login);
+
+
+
+
+public class User
+{
+    public string?  Firstname;
+    public string?  Lastname;
+
+    public string?  Username;
+    public int  Password;
+    
+    public void Login()
+    {
+        if(Username == "Bakhtovar" && Password == 123456)
+        {
+            Console.WriteLine("Welcome mr " + Firstname, Lastname);
+        }
+        else
+        {
+            Console.WriteLine("Login Failed");
+        }
+    }
+
+}
